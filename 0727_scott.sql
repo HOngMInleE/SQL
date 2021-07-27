9d2a6dcf6c0c27b071e6081780c69f1b102d9176
hongmin <82102234+HOngMInleE@users.noreply.github.com>
2021-07-27 17:50:37 +0900

    Add files via upload
    0727 commit은 필수다. 꼭 주기적으로 하자.

---
+++ 0727_scott.sql
@@ -0,0 +1,16 @@
+drop table member;
+
+create table member(
+    name varchar2(30),
+    userid varchar2(30),
+    pwd VARCHAR2(20),
+    emial varchar2(30),
+    phone char(13),
+    admin number(1)
+);
+
+select * from member;
+
+insert into member values ('ÀÌ¼Ò¹Ì','somi','1234','gmd@naver.com','010-765-5412',0);
+insert into member values ('ÇÏ»óÈ£','san12','1234','ha12@naver.com','010-123-1234',0);
+insert into member values ('±èÀ±½Â','light','1234','youn1004@naver.com','010-123-1234',0);
