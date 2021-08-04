b6cea4bb3ba087a159005eb553c4d0a943f873bc
hongmin <82102234+HOngMInleE@users.noreply.github.com>
2021-08-04 17:54:15 +0900

    Add files via upload
    create member for test, product for study.

--- 0727_scott.sql
+++ 0727_scott.sql
@@ -4,7 +4,7 @@ create table member(
     name varchar2(30),
     userid varchar2(30),
     pwd VARCHAR2(20),
-    emial varchar2(30),
+    email varchar2(30),
     phone char(13),
     admin number(1)
 );
@@ -14,3 +14,10 @@ select * from member;
 insert into member values ('ÀÌ¼Ò¹Ì','somi','1234','gmd@naver.com','010-765-5412',0);
 insert into member values ('ÇÏ»óÈ£','san12','1234','ha12@naver.com','010-123-1234',0);
 insert into member values ('±èÀ±½Â','light','1234','youn1004@naver.com','010-123-1234',0);
+
+delete member where name='Makima';
+
+drop table member;
+
+commit;
+
